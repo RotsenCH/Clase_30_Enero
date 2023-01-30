938695c3c870b5b234e2b5cf3fbf564a3ecc3ea4
Nestor <nestor.chumania@epn.edu.ec>
2023-01-30 12:24:01 -0500

    XD

--- src/otromain.java
+++ src/otromain.java
@@ -31,6 +31,7 @@ public class otromain {
                     row[i - 1] = rs.getObject(i);
                 }
                 model.addRow(row);
+                //
             }
 
             // Display JTable in JFrame
